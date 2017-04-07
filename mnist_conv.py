8e35d4b1305e5a90f574874c790e60cd3c394edd
aarniamadeus <aarni.alasaarela@gmail.com>
2017-04-07 14:51:07 +0200

    documentation
    some documentation

--- mnist_conv.py
+++ mnist_conv.py
@@ -12,16 +12,24 @@ import numpy
 
 image = "./threernn.png"
 
+#displays image of choice
 def showImg(image):
     im = Image.open(image)
     im.thumbnail((28, 28), Image.ANTIALIAS)
     imgplot = plt.imshow(im)
     plt.show()
 
+#processes image of choice for model to make prediction
 def preprocessImg(image):
+    #read image file as grayscale matrix
     img = scipy.misc.imread(image, flatten=True, mode='L')
+    
+    #convert to 28 by 28
     img.resize((28, 28))
     img = img.reshape([-1, 28, 28])
+
+    #turn pixel values (0-255) into decimals between 0 and 1
+    #where the larger the number, the darker the pixel (mnist form)
     imgopt = []
     for z in img[0]:
         imgopt.append(["{0:.8f}".format(round(1-i/255,8)) for i in z])
@@ -29,10 +37,12 @@ def preprocessImg(image):
     imgopt = imgopt.reshape([-1, 28, 28])
     return(imgopt)
 
+#xs and ys
 X, Y, test_x, test_y = mnist.load_data(one_hot=True)
 X = X.reshape([-1, 28, 28])
 test_x = test_x.reshape([-1, 28, 28])
 
+#neural net
 convnet = input_data(shape=[None, 28, 28])
 
 convnet = tflearn.lstm(convnet, 64, return_seq=True)
@@ -57,4 +67,6 @@ if __name__ == "__main__":
     model.load('mnistrnn.model')
     showImg(image)
     processedImg = preprocessImg(image)
+    
+    #make prediction on the digit displayed in the image
     print( np.round(model.predict(processedImg)) )
